b7fe77534616feaf0d38b5f5cf5fe86478d04ddc
alexortiz005 <eaortiz@unal.edu.co>
2017-12-12 23:29:44 -0500

    correccion tamaño bannerList

--- Resources/Public/Js/estadistica-unal.js
+++ Resources/Public/Js/estadistica-unal.js
@@ -1,5 +1,45 @@
 $(document).ready(function(){
 
+    function initBannerList(){
+
+        var cantidad_minima = 6
+        var bannerList= $(".bannerList ul.nav-tabs")
+        var cantidad_elementos=bannerList.children().length
+        var cantidad_faltante= cantidad_minima-cantidad_elementos
+
+        if(cantidad_faltante>0){
+
+            if(cantidad_faltante%2==0){
+
+                for (var i = cantidad_faltante/2; i > 0; i--) {
+                    bannerList.prepend('<li></li>');                
+                }
+
+                for (var i = cantidad_faltante/2; i > 0; i--) {
+                    bannerList.append('<li></li>');                
+                }
+
+            }else{
+
+                var cantidad_minima = 7
+                var cantidad_faltante= cantidad_minima-cantidad_elementos
+
+                for (var i = cantidad_faltante/2; i > 0; i--) {
+                    bannerList.prepend('<li></li>');                
+                }
+
+                for (var i = cantidad_faltante/2; i > 0; i--) {
+                    bannerList.append('<li></li>');                
+                }
+
+            }   
+
+        }
+   
+    }
+
+    initBannerList()
+
     $('[data-toggle="tooltip"]').tooltip();
 
     $(".colour-shifter").hover(
@@ -47,7 +87,6 @@ $(document).ready(function(){
 
         });
     });
-
 
 
 });
