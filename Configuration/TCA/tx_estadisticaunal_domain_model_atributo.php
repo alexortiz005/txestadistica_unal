9a395ee53fc19844bb30c26ac1f36efc2488a600
alexortiz005 <eaortiz@unal.edu.co>
2018-02-14 15:15:18 -0500

    correccion tca atributos

--- Configuration/TCA/tx_estadisticaunal_domain_model_atributo.php
+++ Configuration/TCA/tx_estadisticaunal_domain_model_atributo.php
@@ -134,14 +134,17 @@ return [
             'config' => [
                 'type' => 'select',
                 'renderType' => 'selectSingle',
-                'items' => [
-                    ['-- Label --', 0],
+                'items' => [                    
+                    ['LLL:EXT:estadistica_unal/Resources/Private/Language/locallang_db.xlf:tx_estadisticaunal_domain_model_atributo.tipo', '--div--'],
                 ],
+                'foreign_table' => 'tx_estadisticaunal_domain_model_tipoatributo',
+                'foreign_table_where' => 'AND tx_estadisticaunal_domain_model_tipoatributo.pid=###CURRENT_PID###',
                 'size' => 1,
                 'maxitems' => 1,
                 'eval' => ''
             ],
         ],
+    
     
     ],
 ];
