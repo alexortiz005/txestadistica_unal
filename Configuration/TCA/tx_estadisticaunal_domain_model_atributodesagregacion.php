ce4562bb0d7173e86486942c796f918d7b05428e
Alex Ortiz <eaortiz@unal.edu.co>
2018-12-10 10:12:57 -0500

    correccion TCA

--- Configuration/TCA/tx_estadisticaunal_domain_model_atributodesagregacion.php
+++ Configuration/TCA/tx_estadisticaunal_domain_model_atributodesagregacion.php
@@ -133,40 +133,10 @@ return [
             'label' => 'LLL:EXT:estadistica_unal/Resources/Private/Language/locallang_db.xlf:tx_estadisticaunal_domain_model_atributodesagregacion.tipo',
             'config' => [
                 'type' => 'select',
-                'renderType' => 'selectSingleBox',
+                'renderType' => 'selectSingle',
                 'foreign_table' => 'tx_estadisticaunal_domain_model_tipoatrdesagregacion',
-                'MM' => 'tx_estadisticaunal_atributodesagregacion_tipoatrdesagregacion_mm',
-                'size' => 10,
-                'autoSizeMax' => 30,
-                'maxitems' => 9999,
-                'multiple' => 0,
-                'wizards' => [
-                    '_PADDING' => 1,
-                    '_VERTICAL' => 1,
-                    'edit' => [
-                        'module' => [
-                            'name' => 'wizard_edit',
-                        ],
-                        'type' => 'popup',
-                        'title' => 'Edit', // todo define label: LLL:EXT:.../Resources/Private/Language/locallang_tca.xlf:wizard.edit
-                        'icon' => 'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_edit.gif',
-                        'popup_onlyOpenIfSelected' => 1,
-                        'JSopenParams' => 'height=350,width=580,status=0,menubar=0,scrollbars=1',
-                    ],
-                    'add' => [
-                        'module' => [
-                            'name' => 'wizard_add',
-                        ],
-                        'type' => 'script',
-                        'title' => 'Create new', // todo define label: LLL:EXT:.../Resources/Private/Language/locallang_tca.xlf:wizard.add
-                        'icon' => 'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_add.gif',
-                        'params' => [
-                            'table' => 'tx_estadisticaunal_domain_model_tipoatrdesagregacion',
-                            'pid' => '###CURRENT_PID###',
-                            'setValue' => 'prepend'
-                        ],
-                    ],
-                ],
+                'foreign_table_where' => 'AND tx_estadisticaunal_domain_model_tipoatrdesagregacion.pid=###CURRENT_PID###',
+                'MM' => 'tx_estadisticaunal_atributodesagregacion_tipoatrdesagregacion_mm',                
             ],
             
         ],
